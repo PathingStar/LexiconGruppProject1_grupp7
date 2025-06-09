f5936d8225a829ef6ba2ec8e7762667209f30fd1
PathingStar <stanislav.hallberg@hallbergfaux.se>
2025-06-09 09:33:10 +0200

    Add project files.

---
+++ LexiconGruppProject1_grupp7/Program.cs
@@ -0,0 +1,15 @@
+namespace LexiconGruppProject1_grupp7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
